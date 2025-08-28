dcf00e85af9fc0d7b020a9b31f767adb7c00d925
chrizzzii <clvtito@gmail.com>
2025-08-28 16:33:50 +0700

    v1

---
+++ src/data/links.ts
@@ -0,0 +1,13 @@
+import { FaLinkedin, FaGlobe, FaGithub, FaInstagram, FaBullhorn } from "react-icons/fa";
+
+export const links = [
+  {
+    title: "LinkedIn",
+    url: "https://www.linkedin.com/in/agustinusac/",
+    icon: FaLinkedin,
+  },
+  { title: "Website", url: "https://titoapp.cloud/", icon: FaGlobe },
+  { title: "GitHub", url: "https://github.com/chrizzzii", icon: FaGithub },
+  { title: "Instagram", url: "https://instagram.com/advenchristo", icon: FaInstagram },
+  { title: "Feedback", url: "https://feedback.titoapp.cloud/", icon: FaBullhorn },
+];
